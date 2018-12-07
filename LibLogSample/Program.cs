a1bec160bd810768bd42df9d1f134d76472851d6
publicjoe62 <publicjoe62@gmail.com>
2018-12-07 13:39:26 +0100

    Add files via upload
    first commit

---
+++ LibLogSample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using log4net;
+using log4net.Config;
+using MrkSysteme.RapPlugin.Core.Logging;
+
+namespace LibLogSample
+{
+    class MainClass
+    {
+        private static readonly MrkSysteme.RapPlugin.Core.Logging.ILog mLogger = LogProvider.For<MainClass>();
+
+        public static void Main(string[] args)
+        {
+            // configure logger as log4net logger via refelction
+            //
+            XmlConfigurator.Configure(
+                            LogManager.GetRepository(Assembly.GetAssembly(typeof(LogManager))));
+
+            mLogger.Debug("LibLog Sample1 :: start");
+
+            foo.Bar();
+
+            mLogger.Debug("LibLog Sample2 :: end");
+        }
+    }
+}
---
+++ LibLogSample/foo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MrkSysteme.RapPlugin.Core.Logging;
+
+namespace LibLogSample
+{
+    public class foo
+    {
+        private static readonly MrkSysteme.RapPlugin.Core.Logging.ILog mLogger = LogProvider.For<MainClass>();
+
+        public foo()
+        {
+
+        }
+
+        public static void Bar()
+        {
+            mLogger.Info("Bar()");
+        }
+    }
+}
